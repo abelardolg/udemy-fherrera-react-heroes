a84016bba19463a9b9df93b70b337fbdc09a8d2a
Abelardo <diox76@gmail.com>
2020-11-10 18:02:49 +0100

    Adding a second router

---
+++ src/components/dc/DcScreen.js
@@ -0,0 +1,12 @@
+import  React from 'react';
+
+export const DcScreen = () => {
+    return (
+        <>
+            <h1>DC Screen!</h1>
+            <hr/>
+        </>
+    );
+}
+
+
---
+++ src/components/heroes/HeroesScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const HeroesScreen = () => {
+    return (
+        <>
+            <h1>Hello!</h1>
+            <hr/>
+        </>
+    );
+}
+
